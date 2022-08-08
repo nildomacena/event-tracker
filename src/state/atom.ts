db7db58878474dbd6775fc17df166654b826faa9
Ednildo Macena <nildo.777@gmail.com>
2022-08-08 07:42:26 -0300

    event-tracker

---
+++ src/state/atom.ts
@@ -0,0 +1,19 @@
+import { atom } from "recoil";
+import { IEvento } from "../interfaces/IEvento";
+
+export const listaDeEventosState = atom<IEvento[]>({
+    key: 'listaDeEventosState', default: [{
+        "descricao": "Estudar React",
+        "inicio": new Date("2022-08-10T09:00"),
+        "fim": new Date("2022-08-10T13:00"),
+        "completo": false,
+        "id": 1642342747
+    },
+    {
+        "descricao": "Estudar Recoil",
+        "inicio": new Date("2022-08-11T09:00"),
+        "fim": new Date("2022-08-11T11:00"),
+        "completo": false,
+        "id": 1642342959
+    }]
+});
